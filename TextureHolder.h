0f50d0eb04f589266d09e3b050e10ca2f8204d82
2CPT1MP <2CPT1MP@gmail.com>
2020-06-30 00:10:43 +0300

    Added resource management with templates, finished chapter 2

---
+++ TextureHolder.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+namespace Textures
+{
+	enum ID
+	{
+		Landscape, Airplane, Missile
+	};
+}
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+private:
+	std::map<Identifier, std::unique_ptr<Resource>> m_ResourceMap;
+
+public:
+	template <typename Parameter>
+	void load(Identifier id, const std::string& filename, const Parameter& secondParam);
+	Resource& get(Identifier id);
+	const Resource& get(Identifier id) const;
+};
+
