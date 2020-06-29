28e99b9d013d927439c493fe383b4cf8dfea2e72
2CPT1MP <2CPT1MP@gmail.com>
2020-06-29 21:34:05 +0300

    initial commit

--- Game.cpp
+++ Game.cpp
@@ -24,10 +24,19 @@ void Game::processEvents()
 	sf::Event event;
 	while (m_Window.pollEvent(event))
 	{
-		if (event.type == sf::Event::Closed)
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
 			m_Window.close();
+			break;
+		}
 	}
-
 }
 
 void Game::update()
@@ -40,4 +49,17 @@ void Game::render()
 	m_Window.clear();
 	m_Window.draw(m_Player);
 	m_Window.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::W)
+		m_IsMovingUp = true;
+	else if (key == sf::Keyboard::S)
+		m_IsMovingDown = true;
+	else if (key == sf::Keyboard::A)
+		m_IsMovingLeft = true;
+	else if (key == sf::Keyboard::D)
+		m_IsMovingRight = true;
+		
 }
--- Game.h
+++ Game.h
@@ -11,10 +11,15 @@ private:
 	void processEvents();
 	void update();
 	void render();
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
 private:
 	sf::RenderWindow m_Window;
 	sf::CircleShape m_Player;
+	bool m_IsMovingUp;
+	bool m_IsMovingDown;
+	bool m_IsMovingLeft;
+	bool m_IsMovingRight;
 
 };
 
